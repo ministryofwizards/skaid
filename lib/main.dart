f6bf1a99f8215ebacfe80501f66a46b038fddbd3
Namandosi <ndosi248@gmail.com>
2019-07-07 17:27:24 +0530

    Splash Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skaid/screens/splash.dart';
 import 'screens/home.dart';
 
 void main() => runApp(MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: SplashScreen(),
     );
   }
 }
