a151f3ee563a9a28f9f817832f2053838f24075f
kashyapkpatel <kkashyap.patel@gmail.com>
2019-07-07 14:19:18 +0530

    Added scheme model

---
+++ lib/network/model/Scheme.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Scheme {
+
+  String scTitle;
+  String scDesc;
+
+  Scheme.fromSnapShot(DocumentSnapshot snapshot) {
+    scTitle = snapshot['sc_title'];
+    scDesc = snapshot['sc_desc'];
+  }
+}
