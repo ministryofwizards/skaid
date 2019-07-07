a7b0bd121c9db143834f33ceb866bf241b01226d
Namandosi <ndosi248@gmail.com>
2019-07-07 13:57:55 +0530

    gradient background in Scheme

--- lib/screens/schemes.dart
+++ lib/screens/schemes.dart
@@ -10,55 +10,89 @@ class _SchemesState extends State<Schemes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height:30.0,
-          ),
-          Text("Relevant",textAlign:TextAlign.start,style: TextStyle(
-            fontWeight: FontWeight.w300,
-            fontSize: 20,
 
-          ),),
+      body: Stack(
+        children:<Widget>[
           Container(
-
-            height: 300,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.redAccent,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
-              children: <Widget>[
-                ModelCard(),
-                ModelCard(),
-                ModelCard(),
-              ],
-            ),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(58, 28, 113,1),
+              Color.fromRGBO(215, 109, 119,1),
+              Color.fromRGBO(255, 175, 123,1)
+            ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0.2,0.5,0.7]
+             )
           ),
-          Text("All",textAlign:TextAlign.start,style: TextStyle(
-            fontWeight: FontWeight.w300,
-            fontSize: 20,
+        ),
+        Column(
 
-          ),),
-          Container(
-            height: 300,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.blueAccent,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
-              children: <Widget>[
-                ModelCard(),
-                ModelCard(),
-                ModelCard(),
-              ],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height:40.0,
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text("  \n \t \t \t Relevant",textAlign:TextAlign.start,style: TextStyle(
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+                fontSize: 20,
+
+              ),),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
+                  children: <Widget>[
+                    ModelCard(),
+                    ModelCard(),
+                    ModelCard(),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(" \t \t \t All",textAlign:TextAlign.start,style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: Colors.white
+
+              ),),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
+                  children: <Widget>[
+                    ModelCard(),
+                    ModelCard(),
+                    ModelCard(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        ]
       )
     );
   }
